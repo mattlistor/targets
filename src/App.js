18ebffeb6864b9d5b950dc28bc1b45351833b5dd
mattlistor <mattlistor@gmail.com>
2020-01-16 22:10:33 -0500

    here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class App extends React.Component  {
+  state ={
+    points: 0
+  }
 
-function App() {
-  return (
+  click = () => {
+    console.log("clicked!")
+    this.setState({ points: this.state.points + 1 })
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{this.state.points}</div>
+      <br></br>
+      <div className="target" onClick={()=>this.click()}/>
     </div>
-  );
+    );
+  } 
 }
 
 export default App;
+
